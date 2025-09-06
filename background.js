a6bcb52b6f87744058128c40f893288b98940c03
anikaharaguchi720 <anika.haraguchi720@gmail.com>
2025-09-06 18:28:30 +0900

    AI判定ポップアップバグ解消

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const YOUR_OPENAI_API_KEY = "ここにあなたのOpenAI APIキーを入力してください";
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.action === "judgeReflection") {
